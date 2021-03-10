0f85407ca21d07f30509b0c20d1c5711f6ed0d3a
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-10 11:59:50 +0530

    Anurag prakash has added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UC1_Gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MoneyInDollar = 100;
+            int NumberOfGames;
+            Console.WriteLine("Enter the number of gamers");
+            NumberOfGames=Console.Read(); 
+           
+        }
+    }
+}
