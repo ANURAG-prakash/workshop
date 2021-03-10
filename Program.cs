0c6b429541f71f3f7a2c7c48bdd082adf49f17ac
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-10 15:25:37 +0530

    Anurag Prakash

--- Program.cs
+++ Program.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 
-namespace UC1_Gambler
+namespace Workshop
 {
     class Program
     {
+        public const int stack = 100;
+        public const int bet = 1;
+
+
         static void Main(string[] args)
         {
-            int MoneyInDollar = 100;
-            int bet=1
-           Random random = new Random;
+            ToCheckWinOrLoss();
+           ToWinOrLoss();
+            WinLoss20Days();
+          
+        }
+        public static int ToCheckWinOrLoss()
+        {
+            Random random = new Random();
             int result = random.Next(0, 2);
-            if (result == 1)
-                Console.WriteLine("win");
-            else
-                Console.WriteLine("losse");
-           
+            return result;
+        }
+        public static int ToWinOrLoss()
+        {
+            int STACK = stack;
+            int result = ToCheckWinOrLoss();
+            while (STACK < 150 && STACK > 50)
+            {
+                if (result == 0)
+                    STACK = STACK - bet;
+                else if (result == 1)
+                    STACK = STACK + bet;
+            }
+            return STACK;
+        }
+        public static void WinLoss20Days()
+        {
+            int days= ToWinOrLoss();
+            for (int i=1;i<=20;i++)
+            {
+               
+                if (days == 150)
+                    Console.WriteLine(ToWinOrLoss());
+                else if ( days == 50) 
+                    Console.WriteLine(ToWinOrLoss());
+            }
+
         }
     }
 }
+      
