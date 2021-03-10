c0110825828e1971c76bc94db6db0dbf69b3fb96
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-10 13:18:39 +0530

    added

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 
-namespace UC1_Gambler
+namespace Workshop
 {
     class Program
     {
+        public const int stack = 100;
+        public const int bet = 1;
+
+
         static void Main(string[] args)
         {
-            int MoneyInDollar = 100;
-            int NumberOfGames;
-            Console.WriteLine("Enter the number of gamers");
-            NumberOfGames=Console.Read(); 
-           
+            int result = ToCheckWinOrLoss();
+            int profit = 0, loss = 0;
+
+            while (profit <50 && loss <50)
+            {
+                if (result == 1)
+                    profit += 1;
+
+                else if (result == 0)
+                    loss += 1;
+
+            }
+            Console.WriteLine(profit);
+            Console.WriteLine(loss);
+
+
+        }
+        public static int ToCheckWinOrLoss()
+        {
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            return result;
         }
     }
 }
+      
